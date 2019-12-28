1203fb5cb27803201d4f376e779c1df06afbefa1
unknown <cryptojeju@gmail.com>
2019-12-28 16:23:21 +0900

    test

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.eb8885abd43e4a7491609351653d4cf5.js"
+  "./precache-manifest.57bd79853447d871046442de4527ca3a.js"
 );
 
 workbox.clientsClaim();
